d5b9a9139f1398ad3136a425dce22d63b76e9757
Olga Lozovik <lozovikolga@gmail.com>
2016-03-30 01:55:34 +0300

    Task2

---
+++ Task2.NUnit.Tests/FibonacciTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using static Task2.Fibonacci;
+using NUnit.Framework;
+
+namespace Task2.NUnit.Tests
+{
+    [TestFixture]
+    public class FibonacciTests
+    {
+        [TestCase(ExpectedException = typeof(ArgumentException))]
+        public void CountFibonacci_BadIndex()
+        {
+            IEnumerable<int> actual = CountFibonacci(0);
+            Assert.AreEqual(new List<int>(), actual);
+        }
+
+        [TestCase]
+        public void CountFibonacci_OneNumber()
+        {
+            IEnumerable<int> expected = new List<int> { 1 };
+            IEnumerable<int> actual = CountFibonacci(1);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase]
+        public void CountFibonacci_ManyNumbers()
+        {
+            IEnumerable<int> expected = new List<int> {1, 1, 2, 3, 5, 8, 13, 21, 34};
+            IEnumerable<int> actual = CountFibonacci(9);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Task2/Fibonacci.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task2
+{
+    public static class Fibonacci
+    {
+        public static IEnumerable<int> CountFibonacci(int position)
+        {
+            if (position <= 0)
+            {
+                throw new ArgumentException("Fibonacci number position must be above zero!");
+            }
+
+            int number = 0;
+            int lastNumber = 1;
+
+            for (int i = 0; i < position; i++)
+            {
+                int tempNumber = number;
+                number += lastNumber;
+                lastNumber = tempNumber;
+
+                yield return number;
+            }
+        }
+    }
+}
